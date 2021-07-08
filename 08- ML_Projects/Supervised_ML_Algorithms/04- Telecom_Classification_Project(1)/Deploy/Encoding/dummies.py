e6b4416c84d312b897ca9c5efb1047e91a204c44
Ahmed <ahmed.elsayed862@yahoo.com>
2021-07-08 17:48:50 +0200

    update

---
+++ 08- ML_Projects/Supervised_ML_Algorithms/04- Telecom_Classification_Project(1)/Deploy/Encoding/dummies.py
@@ -0,0 +1,124 @@
+
+Gender_dumm = {
+	'Male' : [1] ,
+	'Female' : [0]
+}
+
+
+
+
+Partner_dumm = {
+	"Yes" : [1] ,
+	"No" : [0]
+}
+
+
+
+
+
+Dependents_dumm = {
+	"Yes" : [1] ,
+	"No" : [0]
+}
+
+
+
+PhoneService_dumm = {
+	"Yes" : [1] ,
+	"No" : [0]
+}
+
+
+
+
+MultipleLines_dumm = {
+	
+	'No phone service' : [1 , 0] ,
+	'Yes' : [0 ,1] ,
+	'No' : [0,0]
+
+					}
+
+InternetService_dumm = {
+	
+	'Fiber optic' : [1 , 0] ,
+	'No'  :  [0 , 1] , 
+	'DSL' : [0 , 0]
+
+}
+
+
+OnlineSecurity_dumm = {
+	
+	"No internet service" : [1 , 0] , 
+	"Yes" : [0 , 1] ,
+	"No"  : [0 , 0]
+}
+
+
+OnlineBackup_dumm = {
+	"No internet service" : [1 , 0] , 
+	"Yes" : [0 , 1] ,
+	"No"  : [0 , 0]
+}
+
+DeviceProtection_dumm = {
+	"No internet service" : [1 , 0] ,
+	"Yes" : [0,1] ,
+	"No" : [0,0]
+}
+
+
+
+TechSupport_dumm = {
+	"No internet service" : [1 , 0] ,
+	"Yes" : [0 , 1] ,
+	"No" : [0 , 0]
+}
+
+
+StreamingTV_dumm = {
+	"No internet service" : [1 , 0] ,
+	"Yes" : [0 , 1] ,
+	"No" : [0 , 0]
+}
+
+
+
+StreamingMovies_dumm = {
+	"No internet service" : [1 , 0] ,
+	"Yes" : [0 , 1] ,
+	"No" : [0 , 0]
+}
+
+
+
+Contract_dumm = {
+	'One year' : [1 , 0] ,
+	'Two year' : [0 , 1] ,
+	'Month-to-month' : [0 , 0]
+}
+
+
+
+PaperlessBilling_dumm = {
+	"Yes" : [1] ,
+	"No" : [0]
+}
+
+
+
+PaymentMethod_dumm = {
+	"No internet service" : [1 , 0] ,
+	"Yes" : [0 , 1] ,
+	"No" : [0 , 0]
+}
+
+
+TechSupport_dumm = {
+	'Credit card (automatic)' : [1 , 0 , 0] ,
+	'Electronic check' : [0 , 1 , 0] ,
+	'Mailed check' : [0 , 0 , 1] ,
+	'Bank transfer (automatic)' : [0,0,0]
+}
+
