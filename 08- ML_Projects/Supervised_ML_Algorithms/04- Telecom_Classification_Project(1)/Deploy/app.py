b15b4fcc911b973d03309753263e1c2fe8ff36c4
Ahmed <ahmed.elsayed862@yahoo.com>
2021-07-10 18:44:04 +0200

    update

--- 08- ML_Projects/Supervised_ML_Algorithms/04- Telecom_Classification_Project(1)/Deploy/app.py
+++ 08- ML_Projects/Supervised_ML_Algorithms/04- Telecom_Classification_Project(1)/Deploy/app.py
@@ -1,57 +1,164 @@
+from flask import Flask , render_template , request
 import joblib
-from Encoding.dummies import *
-from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 model = joblib.load('models/Model.h5')
 scaler = joblib.load('models/scaler.h5')
 
 
+genders = ["Male"]
+Partners = ['Yes']
+Dependentss = ["Yes"]
+PhoneServices = ["Yes"]
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
+MultipleLiness = ['No phone service', 'Yes']
+InternetServices =['Fiber optic', 'No']
+OnlineSecuritys = ['No internet service', 'Yes']
+OnlineBackups = ['No internet service', 'Yes']
+DeviceProtections =  ['No internet service', 'Yes']
+TechSupports = ['No internet service', 'Yes']
+StreamingTVs = ['No internet service', 'Yes']
+StreamingMoviess = ['No internet service', 'Yes']
+Contracts= ['One year', 'Two year']
+PaperlessBillings = ["Yes"]
+PaymentMethods = ['Credit card (automatic)','Electronic check', 'Mailed check']
 
 
-@app.route('/predict', methods=['GET'])
-def predict():
-    all_data       = request.args
-    SeniorCitizen  = int(all_data['SeniorCitizen'])
-    tenure         = int(all_data['tenure'])
-    MonthlyCharges = float(all_data['MonthlyCharges'])
-    TotalCharges   = float(all_data['TotalCharges'])
+@app.route("/" , methods=["GET"])
+def home():
+    return render_template("index.html")
+
+
+
+
+@app.route("/Predict" , methods =["GET"])
+def prediction() :
+
+    inp_data = [
+        request.args.get("SeniorCitizen") ,
+        request.args.get("tenure") ,
+        request.args.get("MonthlyCharges") ,
+        request.args.get("TotalCharges")]
+    g    = [0]
+    p    = [0]
+    D    = [0]
+    P_S  = [0]
+    M_l  = [0 for i in range(2)]
+    I_S  = [0 for i in range(2)]
+    O_S  = [0 for i in range(2)]
+    O_B  = [0 for i in range(2)]
+    D_P  = [0 for i in range(2)]
+    T_S  = [0 for i in range(2)]
+    S_T  = [0 for i in range(2)]
+    S_M  = [0 for i in range(2)]
+    cont = [0 for i in range(2)]
+    Pap  = [0]
+    P_M  = [0 for i in range(3)]
+    try :
+        g[genders.index(request.args.get("gender"))] = 1
+        
+    except :
+        pass
+    try :
+        p[Partners.index(request.args.get("Partner"))] = 1
+        
+    except :
+        pass 
+    try :
+        D[Dependentss.index(request.args.get("Dependents"))] = 1
+        
+    except :
+        pass
+    try :
+        P_S[PhoneServices.index(request.args.get("PhoneService"))] = 1
+        
+    except :
+        pass
+    try :
+        M_l[MultipleLiness.index(request.args.get("MultipleLines"))] = 1
+        
+    except :
+        pass
+    try :
+        I_S[InternetServices.index(request.args.get("InternetService"))] = 1
+        
+    except :
+        pass
+    try :
+        O_S[OnlineSecuritys.index(request.args.get("OnlineSecurity"))] = 1
+        
+    except :
+        pass
+    try :
+        O_B[OnlineBackups.index(request.args.get("OnlineBackup"))] = 1
+    except :
+        pass
+    try :
+        D_P[DeviceProtections.index(request.args.get("DeviceProtection"))] = 1
+        
+    except :
+        pass
+    try :
+        T_S[TechSupports.index(request.args.get("TechSupport"))] = 1
+        
+    except :
+        pass
+    try :
+        S_T[StreamingTVs.index(request.args.get("StreamingTV"))] = 1
+        
+    except :
+        pass
+    try :
+        S_M[StreamingMoviess.index(request.args.get("StreamingMovies"))] = 1
+        
+    except :
+        pass
+    try :
+        cont[Contracts.index(request.args.get("Contract"))] = 1
+        
+    except :
+        pass
+    try :
+        Pap[PaperlessBillings.index(request.args.get("PaperlessBilling"))] = 1
+        
+    except :
+        pass
+    try :
+        P_M[PaymentMethods.index(request.args.get("PaymentMethod"))] = 1
+        
+    except :
+        pass
 
     
-    gender           = Gender_dumm[all_data['gender']]
-    Partner          = Partner_dumm[all_data['Partner']]
-    Dependents       = Dependents_dumm[all_data['Dependents']]
-    PhoneService     = PhoneService_dumm[all_data['PhoneService']]
-    MultipleLines    = MultipleLines_dumm[all_data['MultipleLines']]
-    InternetService  = InternetService_dumm[all_data['InternetService']]
-    OnlineSecurity   = OnlineSecurity_dumm[all_data['OnlineSecurity']]
-    OnlineBackup     = OnlineBackup_dumm[all_data['OnlineBackup']]
-    DeviceProtection = DeviceProtection_dumm[all_data['DeviceProtection']]
-    TechSupport      = TechSupport_dumm[all_data['TechSupport']]
-    StreamingTV      = StreamingTV_dumm[all_data['StreamingTV']]
-    StreamingMovies  = StreamingMovies_dumm[all_data['StreamingMovies']]
-    Contract         = Contract_dumm[all_data['Contract']]
-    PaperlessBilling = PaperlessBilling_dumm[all_data['PaperlessBilling']]
-    PaymentMethod    = PaymentMethod_dumm[all_data['PaymentMethod']]
+    inp_data+=g
+    inp_data+=p
+    inp_data+=D
+    inp_data+=P_S
+    inp_data+=M_l
+    inp_data+=I_S
+    inp_data+=O_S
+    inp_data+=O_B
+    inp_data+=D_P
+    inp_data+=T_S
+    inp_data+=S_T
+    inp_data+=S_M
+    inp_data+=cont
+    inp_data+=Pap
+    inp_data+=P_M
+
+    inp_data = [int(n) for n in inp_data]
+
+    Churn = model.predict(scaler.transform([inp_data]))[0]
+    
+    return render_template("prediction.html" , Churn = Churn)
+    
 
 
 
 
 
-    x = [SeniorCitizen, tenure, MonthlyCharges, TotalCharges, gender , Partner , Dependents , PhoneService]
-    x += MultipleLines + InternetService + OnlineSecurity + OnlineBackup + DeviceProtection 
-    x +=  TechSupport + StreamingTV + StreamingMovies + Contract + PaperlessBilling + PaymentMethod
-
-
-    x = scaler.transform([x])
-    churn  = model.predict(x)[0]
-
-    return render_template('prediction.html', Churn = churn)
 
 
 
@@ -60,3 +167,4 @@ def predict():
 
 if __name__ == '__main__':
     app.run(debug = True , host="127.0.0.1")
+
---
+++ 08- ML_Projects/Supervised_ML_Algorithms/04- Telecom_Classification_Project(1)/app.py
@@ -0,0 +1,63 @@
+import joblib
+from Encoding.dummies import *
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+model = joblib.load('models/Model.h5')
+scaler = joblib.load('models/scaler.h5')
+
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET'])
+def prediction():
+    all_data       = request.args
+    SeniorCitizen  = int(all_data['SeniorCitizen'])
+    tenure         = int(all_data['tenure'])
+    MonthlyCharges = float(all_data['MonthlyCharges'])
+    TotalCharges   = float(all_data['TotalCharges'])
+
+    
+    gender           = Gender_dumm[all_data['gender']]
+    Partner          = Partner_dumm[all_data['Partner']]
+    Dependents       = Dependents_dumm[all_data['Dependents']]
+    PhoneService     = PhoneService_dumm[all_data['PhoneService']]
+    MultipleLines    = MultipleLines_dumm[all_data['MultipleLines']]
+    InternetService  = InternetService_dumm[all_data['InternetService']]
+    OnlineSecurity   = OnlineSecurity_dumm[all_data['OnlineSecurity']]
+    OnlineBackup     = OnlineBackup_dumm[all_data['OnlineBackup']]
+    DeviceProtection = DeviceProtection_dumm[all_data['DeviceProtection']]
+    TechSupport      = TechSupport_dumm[all_data['TechSupport']]
+    StreamingTV      = StreamingTV_dumm[all_data['StreamingTV']]
+    StreamingMovies  = StreamingMovies_dumm[all_data['StreamingMovies']]
+    Contract         = Contract_dumm[all_data['Contract']]
+    PaperlessBilling = PaperlessBilling_dumm[all_data['PaperlessBilling']]
+    PaymentMethod    = PaymentMethod_dumm[all_data['PaymentMethod']]
+
+
+
+
+
+    x = [SeniorCitizen, tenure, MonthlyCharges, TotalCharges, gender , Partner , Dependents , PhoneService]
+    x += MultipleLines + InternetService + OnlineSecurity + OnlineBackup + DeviceProtection 
+    x +=  TechSupport + StreamingTV + StreamingMovies + Contract + PaperlessBilling + PaymentMethod
+
+
+    #x = scaler.transform([x])
+    #churn  = model.predict(x)[0]
+
+    #return render_template('prediction.html', Churn = churn)
+    return str(PaymentMethod)
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True , host="127.0.0.1")
